fcaa24ca3fe2636e374387d1764167f51d61ddd8
Makpal <maqpal.rus@gmail.com>
2025-04-26 11:58:58 +0500

    basic Interfaces

--- src/part2/Aircraft.java
+++ src/part2/Aircraft.java
@@ -1,4 +1,22 @@
 package part2;
 
-public class Aircraft {
+public abstract class Aircraft {
+    protected String id;
+    protected int fuelLevel;
+    private String msg;
+
+    public Aircraft(String id, int fuel) {
+        this.id = id;
+        this.fuelLevel = fuel;
+    }
+
+    public abstract void receive(String msg);
+
+    public void send(String msg, TowerMediator m) {
+        m.broadcast(msg, this);
+    }
+
+    public boolean isEmergency() {
+        return fuelLevel < 10 || msg.equals("MAYDAY");
+    }
 }
--- src/part2/TowerMediator.java
+++ src/part2/TowerMediator.java
@@ -1,4 +1,6 @@
 package part2;
 
-public class TowerMediator {
+public interface TowerMediator {
+    void broadcast(String msg, Aircraft sender);
+    boolean requestRunway(Aircraft a);
 }
